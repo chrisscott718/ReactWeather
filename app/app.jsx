53ae60c38d4c0f7fb3a06e688221332463731fc9
Chris Scott <chris.scott718@gmail.com>
2017-05-23 16:58:01 -0700

    add nav search functionality

--- app/app.jsx
+++ app/app.jsx
@@ -7,10 +7,10 @@ var About = require('About');
 var Examples = require('Examples');
 
 // load foundation-sites
-require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation.min.css')
 
 // app css
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 $(document).foundation();
 
 ReactDOM.render(
